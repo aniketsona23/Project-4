027d3084a5cd5b52cc58c42852b7deb25e5f9322
Aniket Sonawane <72734761+aniketsona23@users.noreply.github.com>
2021-01-16 17:43:47 +0530

    Add files via upload

---
+++ login_system.js
@@ -0,0 +1,21 @@
+const validator = require("validator")
+const chalk = require("chalk")
+
+const users = {
+    name: "Aniket",
+    valid_username: "anky01",
+    valid_password: "password-aniket"
+}
+const username = process.argv[2]
+const password = process.argv[3]
+
+console.log(chalk.bold.hex("#ffee58")("-------------- Intra Login System ------------"))
+if (username == users.valid_username) {
+    if (password == users.valid_password) {
+        console.log(chalk.hex("108006").bold.bgHex("ffee58")("Welcome", users.name))
+    } else {
+        console.log(chalk.red.bold("Invalid Password!"))
+    }
+} else {
+    console.log(chalk.red.bold("Invalid Username!"))
+}
