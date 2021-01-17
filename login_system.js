0ce45be9f90dc88fe5ab77715f19762b2ea05254
Aniket Sonawane <72734761+aniketsona23@users.noreply.github.com>
2021-01-17 20:24:47 +0530

    Update login_system.js

--- login_system.js
+++ login_system.js
@@ -1,20 +1,39 @@
 const chalk = require("chalk")
+const validator = require("validator")
 
 const users = {
     name: "Aniket",
-    valid_username: "anky01",
+    valid_username: "anky01@gmail.com",
     valid_password: "password-aniket"
 }
 const username = process.argv[2]
 const password = process.argv[3]
 
+
 console.log(chalk.bold.hex("#ffee58")("-------------- Intra Login System ------------"))
-if (username == users.valid_username) {
-    if (password == users.valid_password) {
-        console.log(chalk.hex("108006").bold.bgHex("ffee58")("Welcome", users.name))
+
+if (username == undefined) {
+
+    console.log("[-] Please Enter Username")
+
+} else if (password == undefined) {
+
+    console.log("[-] Please Enter Password")
+
+} else if (validator.isEmail(username) == true) {
+
+    if (username == users.valid_username) {
+
+        if (password == users.valid_password) {
+            console.log(chalk.hex("108006").bold.bgHex("ffee58")("Welcome", users.name))
+        } else {
+            console.log(chalk.red.bold("Invalid Password!"))
+        }
+
     } else {
-        console.log(chalk.red.bold("Invalid Password!"))
+        console.log(chalk.red.bold("Invalid Username!"))
     }
+
 } else {
-    console.log(chalk.red.bold("Invalid Username!"))
+    console.log("Please Enter Username in Valid Format, eg: abc@email.com")
 }
